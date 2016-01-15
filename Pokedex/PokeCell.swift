00cf717c3cf1a199bc7515e5a435d0e83a85e359
Cathleen Topness <itunes@topness.net>
2016-01-14 20:18:17 -0500

    Adding in music.

---
+++ Pokedex/PokeCell.swift
@@ -0,0 +1,32 @@
+//
+//  PokeCell.swift
+//  Pokedex
+//
+//  Created by Topness on 1/13/16.
+//  Copyright © 2016 Symmary. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var _pokemon: Pokemon!
+    
+
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        self.layer.cornerRadius = 4.0
+        
+    }
+    
+    func configureCell(pokemon: Pokemon) {
+        _pokemon = pokemon
+        nameLbl.text = _pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(_pokemon.pokedexId)")
+    }
+    
+    
+}
--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -7,14 +7,83 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var collection: UICollectionView!
+    
+    var pokemon = [Pokemon]()
+    var musicPlayer: AVAudioPlayer
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        collection.delegate = self
+        collection.dataSource = self
+        
+        initAudio()
+        parsePokemonCSV()
+    }
+    
+    func parsePokemonCSV() {
+        let path = NSBundle.mainBundle().pathForResource("pokemon", ofType: "csv")!
+        
+        do {
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows
+            
+            for row in rows {
+                let pokeID = Int(row["id"]!)!
+                let name = row["identifier"]!
+
+                pokemon.append(Pokemon(name: name, pokedexId: pokeID))
+            }
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        if let cell = collectionView.dequeueReusableCellWithReuseIdentifier("PokeCell", forIndexPath: indexPath) as? PokeCell{
+            let thisPokemon = pokemon[indexPath.row]
+            cell.configureCell(thisPokemon)
+            return cell
+        } else {
+            return UICollectionViewCell()
+        }
+        
+    }
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return pokemon.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        //placeholder
+        
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        
+        return CGSizeMake(105, 105)
+        
+    }
+    
+    func initAudio() {
+        let path = NSBundle.mainBundle().pathForResource(<#T##name: String?##String?#>, ofType: <#T##String?#>)
     }
 
+    @IBAction func musicButtonPressed(sender: AnyObject) {
+        
+    }
 
 }
 
